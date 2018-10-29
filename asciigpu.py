02269b2b6c517cf2782e00b1368fd14ff76bb968
Michael Auerswald <C:\Users\michael\AppData\Roaming\The Bat!>
2018-10-29 21:50:05 +0100

    more fixes

--- asciigpu.py
+++ asciigpu.py
@@ -76,8 +76,28 @@ class GpuInfoModel(object):
 
     def get_columns(self):
         ret = [([x.toString(), x.getUsageAsBar(), str(x.getUsage()), str(x.getTemperature())], x.index) for x in self._gpus]
-        # print(ret)
         return ret
+    
+    def get_detail(self, index):
+        details = []
+        i = 0
+        details.append((["Name", "{0}".format(getGpuData(index, "type").decode('UTF-8'))], i))
+        i+=1
+        details.append((["Utilization (GPU)", "{0} %".format(getGpuData(index, "util"))], i))
+        i+=1
+        details.append((["Utilization (MEM)", "{0} %".format(getGpuData(index, "mem_util"))], i))
+        i+=1
+        details.append((["GPU Clock Speed", "{0} MHz".format(getGpuData(index, "graphics_clock_report"))], i))
+        i+=1
+        details.append((["Memory Clock Speed", "{0} MHz".format(getGpuData(index, "mem_clock_report"))], i))
+        i+=1
+        details.append((["Temperature", "{0} C".format(getGpuData(index, "temp"))], i))
+        i+=1
+        details.append((["UUID", "{0}".format(getGpuData(index, "uuid").decode('UTF-8'))], i))
+        i+=1
+        details.append((["Memory (total)", "{0} MB".format(getGpuData(index, "mem_total"))], i))
+        i+=1
+        return details
 
 class MainView(Frame):
     _currentPick = 0
@@ -94,7 +114,7 @@ class MainView(Frame):
                                     title="asciiGPU {0} (Michael Auerswald @ 908video GmbH)".format(_version))
         # Save off the model that accesses the contacts database.
         self._model = model
-        self._refresh = _defaultRefresh
+        self._refresh = _refreshRate
 
         layout = Layout([1], fill_frame=True)
         self.add_layout(layout)
@@ -110,7 +130,7 @@ class MainView(Frame):
         layout.add_widget(self._list_view)
         layout2 = Layout([1, 1, 1, 1])
         self.add_layout(layout2)
-        self._refresh_label = Label("Refresh(s): {0:.2f}".format(self._refresh))
+        self._refresh_label = Label("Refresh(s): {0:.2f}".format(_refreshRate))
         layout2.add_widget(Button("Quit", self._quit), 0)
         layout2.add_widget(self._refresh_label, 1)
         # layout2.add_widget(Button("-", self._refresh_minus), 2)
@@ -122,21 +142,23 @@ class MainView(Frame):
         self.fix()
         self._on_pick()
 
-    def _refresh_minus(self):
-        self._refresh = max(0.1,self._refresh-0.1)
-        self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
-        self._reload_list()
+    # def _refresh_minus(self):
+    #     self._refresh = max(0.1,self._refresh-0.1)
+    #     self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
+    #     self._reload_list()
     
-    def _refresh_plus(self):
-        self._refresh = max(0.1,self._refresh+0.1)
-        self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
-        self._reload_list()
+    # def _refresh_plus(self):
+    #     self._refresh = max(0.1,self._refresh+0.1)
+    #     self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
+    #     self._reload_list()
 
     def _on_pick(self):
         self._currentPick = self._list_view.value
 
     def _on_select(self):
-        index = self._currentPick
+        global _currentIndex, _detailIsOpen
+        _detailIsOpen = True
+        _currentIndex = self._currentPick
         raise NextScene("Detail")
 
     def _reload_list(self, new_value=None):
@@ -144,16 +166,12 @@ class MainView(Frame):
         temp = self._currentPick
         self._list_view.options = self._model.get_columns()
         self._list_view.value = temp
+        self._refresh_label.text = "Refresh(s): {0:.2f}".format(_refreshRate)
         _screen.force_update()
         if _updateThread and _updateThread.is_alive:
             _updateThread.cancel()
-        _updateThread = Timer(self._refresh, self._reload_list)
+        _updateThread = Timer(_refreshRate, self._reload_list)
         _updateThread.start()
-
-    def _show_detail(self):
-        global _detailIsOpen
-        _detailIsOpen = True
-        pass
 
     @staticmethod
     def _quit():
@@ -162,7 +180,7 @@ class MainView(Frame):
 
 class DetailView(Frame):
     def __init__(self, screen, model):
-        global _detailIsOpen
+        global _detailIsOpen, _currentIndex
         _detailIsOpen = True
         super(DetailView, self).__init__(screen,
                                     int(screen.height * 0.8),
@@ -173,43 +191,58 @@ class DetailView(Frame):
                                     title="Detail View")
         # Save off the model that accesses the contacts database.
         self._model = model
-        self._refresh = _defaultRefresh
+        self._refresh = _refreshRate
 
         layout = Layout([1], fill_frame=True)
         self.add_layout(layout)
 
-        # Create the form for displaying the list of contacts.
-        self._list_view = MultiColumnListBox(
+        self._list_detail_view = MultiColumnListBox(
             height=Widget.FILL_FRAME,
-            columns=[1,1],
-            options=model.get_columns(),
+            columns=["<50%","<50%"],
+            options=model.get_detail(_currentIndex),
             titles=["Parameter", "Value"],
             on_change=self._on_pick)
-        layout.add_widget(self._list_view)
+        layout.add_widget(self._list_detail_view)
         layout2 = Layout([1, 1, 1, 1])
         self.add_layout(layout2)
+        self._refresh_label = Label("Refresh(s): {0:.2f}".format(_refreshRate))
         layout2.add_widget(Button("close", self._cancel), 0)
+        layout2.add_widget(self._refresh_label, 1)
         layout3 = Layout([1])
         self.add_layout(layout3)
-        layout3.add_widget(Label("Press 'Esc' to close.", align="^"), 0)
+        layout3.add_widget(Label("Press 'Esc' to close. '+'/'-' to change refresh rate.", align="^"), 0)
+
+    # def _refresh_minus(self):
+    #     self._refresh = max(0.1,self._refresh-0.1)
+    #     self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
+    #     self._reload_list()
+    
+    # def _refresh_plus(self):
+    #     self._refresh = max(0.1,self._refresh+0.1)
+    #     self._refresh_label.text = "Refresh(s): {0:.2f}".format(self._refresh)
+    #     self._reload_list()
 
         self.fix()
         self._on_pick()
 
     def _on_pick(self):
-        pass
-    #     self._edit_button.disabled = self._list_view.value is None
-    #     self._delete_button.disabled = self._list_view.value is None
+        self._currentPick = self._list_detail_view.value
+
+    # def _on_select(self):
+    #     global _currentIndex
+    #     _currentIndex = self._currentPick
+    #     raise NextScene("Detail")
 
     def _reload_list(self, new_value=None):
-        global _updateThread
-        self._list_view.options = self._model.get_columns()
-        self._list_view.value = new_value
-        # _screen.force_update()
+        global _updateThread, _currentIndex
+        temp = self._currentPick
+        self._list_detail_view.options = self._model.get_detail(_currentIndex)
+        self._list_detail_view.value = temp
+        _screen.force_update()
 
         if _updateThread and _updateThread.is_alive:
             _updateThread.cancel()
-        _updateThread = Timer(self._refresh, self._reload_list)
+        _updateThread = Timer(_refreshRate, self._reload_list)
         _updateThread.start()
 
     @staticmethod
@@ -228,15 +261,28 @@ def shutdown():
     sys.exit(0)
 
 def global_shortcuts(event):
-    global _mainView, _detailIsOpen
+    global _mainView, _detailIsOpen, _refreshRate
     if isinstance(event, KeyboardEvent):
         c = event.key_code
-        print(c)
+        # print(c)
         # Stop on ctrl+q or ctrl+x
         if c == 45:
-            _mainView._refresh_minus()
+            _refreshRate = max(0.1, _refreshRate-0.1)
+            if _detailIsOpen == False:
+                # _mainView._refresh_minus()
+                _mainView._refresh_label.text = "Refresh(s): {0:.2f}".format(_refreshRate)
+            else:
+                # _detailView._refresh_minus()
+                _detailView._refresh_label.text = "Refresh(s): {0:.2f}".format(_refreshRate)
         elif c == 43:
-            _mainView._refresh_plus()
+            _refreshRate = max(0.1, _refreshRate+0.1)
+            if _detailIsOpen == False:
+            #     _mainView._refresh_plus()
+                _mainView._refresh_label.text = "Refresh(s): {0:.2f}".format(_refreshRate)
+            else:
+            #     _detailView._refresh_minus()
+                _detailView._refresh_label.text = "Refresh(s): {0:.2f}".format(_refreshRate)
+
         elif c == 113 or c == -1:
             if _detailIsOpen == False:
                 raise StopApplication("User terminated app")
@@ -247,10 +293,11 @@ def global_shortcuts(event):
             raise StopApplication("User terminated app")
 
 def main(screen, scene):
-    global _mainView, _detailView, _screen
+    global _mainView, _detailView, _screen, _detailIsOpen
     _screen = screen
     _mainView = MainView(screen, _gpuinfo)
     _detailView = DetailView(screen, _gpuinfo)
+    _detailIsOpen = False
     scenes = [
         Scene([_mainView], -1, name="Main"),
         Scene([_detailView], -1, name="Detail")
@@ -266,13 +313,14 @@ def main(screen, scene):
 
 last_scene = None
 _version = 0.11
-_defaultRefresh = 0.5
+_refreshRate = 0.5
 _Gpus = []
 _screen = None
 _updateThread = None
 _gpuinfo = GpuInfoModel()
 _mainView = None
 _detailView = None
+_currentIndex = 0
 _detailIsOpen = False
 while True:
     try:
